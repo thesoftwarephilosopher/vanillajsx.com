8670bc0a66e3ee076b3a37df9578b5da8f2aee6d
sdegutis <sbdegutis@gmail.com>
2024-08-16 09:51:14 -0500

    They literally all are modules.

--- site/[samples].tsx.ts
+++ site/[samples].tsx.ts
@@ -1,9 +1,4 @@
 import * as path from 'path';
 import files from './samples/';
 
-export default (files
-  .filter(f => f.module !== undefined) // todo: fix usage of ts 5.5 fix
-  .map(f => {
-    return ['samples/' + path.basename(f.path.slice(0, -3)), f.module!.source];
-  })
-);
+export default (files.map(f => ['samples/' + path.basename(f.path.slice(0, -3)), f.module!.source]));
