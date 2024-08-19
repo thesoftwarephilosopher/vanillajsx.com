b21df07bfb497be37a2c814f2f7ceab338de9807
sdegutis <sbdegutis@gmail.com>
2024-08-19 10:36:16 -0500

    Use a function.

--- site/vanillajsx/compiler.ts
+++ site/vanillajsx/compiler.ts
@@ -1,52 +1,48 @@
 import * as swc from '@swc/core';
 
-export class Compiler {
+export function compile(code: string, browserFilePath: string) {
+  let prefix = '';
+  if (!browserFilePath.startsWith('/@imlib/')) {
+    const levels = browserFilePath.match(/\//g)!.length - 1;
+    prefix = '.' + '/..'.repeat(levels);
+  }
 
-  compile(code: string, browserFilePath: string) {
-    let prefix = '';
-    if (!browserFilePath.startsWith('/@imlib/')) {
-      const levels = browserFilePath.match(/\//g)!.length - 1;
-      prefix = '.' + '/..'.repeat(levels);
-    }
-
-    const opts: swc.Options = {
-      sourceMaps: 'inline',
-      module: { type: 'es6' },
-      plugin: (program) => {
-        return this.#renameImports(program);
+  const opts: swc.Options = {
+    sourceMaps: 'inline',
+    module: { type: 'es6' },
+    plugin: (program) => {
+      return renameImports(program);
+    },
+    jsc: {
+      parser: {
+        syntax: 'typescript',
+        tsx: true,
       },
-      jsc: {
-        parser: {
-          syntax: 'typescript',
-          tsx: true,
-        },
-        target: 'esnext',
-        transform: {
-          react: {
-            runtime: 'automatic',
-            importSource: '/@imlib',
-            throwIfNamespace: false,
-          }
-        }
-      }
-    };
-
-    const result = swc.transformSync(code, opts);
-    result.code = result.code.replace(/"\/@imlib\/jsx-runtime"/g, `"${prefix}/@imlib/jsx-browser.js"`);
-    return result;
-  }
-
-  #renameImports(program: swc.Program): swc.Program {
-    for (const imp of program.body) {
-      if (imp.type === 'ImportDeclaration') {
-        const dep = imp.source.value;
-        if (!dep.match(/^[./]/)) {
-          delete imp.source.raw;
-          imp.source.value = `https://cdn.jsdelivr.net/npm/${dep}/+esm`;
+      target: 'esnext',
+      transform: {
+        react: {
+          runtime: 'automatic',
+          importSource: '/@imlib',
+          throwIfNamespace: false,
         }
       }
     }
-    return program;
-  }
+  };
 
+  const result = swc.transformSync(code, opts);
+  result.code = result.code.replace(/"\/@imlib\/jsx-runtime"/g, `"${prefix}/@imlib/jsx-browser.js"`);
+  return result;
+}
+
+function renameImports(program: swc.Program): swc.Program {
+  for (const imp of program.body) {
+    if (imp.type === 'ImportDeclaration') {
+      const dep = imp.source.value;
+      if (!dep.match(/^[./]/)) {
+        delete imp.source.raw;
+        imp.source.value = `https://cdn.jsdelivr.net/npm/${dep}/+esm`;
+      }
+    }
+  }
+  return program;
 }
